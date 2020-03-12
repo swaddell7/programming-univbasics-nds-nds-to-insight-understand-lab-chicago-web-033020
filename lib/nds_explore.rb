349e9eb3bb61fbb81ad69b41da4ee695c06696bb
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-12 20:05:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,4 +9,15 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  
+  row_index = 0
+  while row_index < directors_database.count do
+    element_index = 0 
+    while element_index < directors_database[row_index].count do
+      puts directors_database[row_index][element_index]
+      element_index += 1 
+    end
+    row_index += 1
+  end
+  
 end
